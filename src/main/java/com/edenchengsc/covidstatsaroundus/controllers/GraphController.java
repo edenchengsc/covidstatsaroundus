36187b5f9ab160185ec4845315cbae7dc85ba697
edenchengsc <eden.polyu@gmail.com>
2021-06-15 00:48:02 -0700

    Checkin map chart sample

--- src/main/java/com/edenchengsc/covidstatsaroundus/controllers/GraphController.java
+++ src/main/java/com/edenchengsc/covidstatsaroundus/controllers/GraphController.java
@@ -1,13 +1,19 @@
 package com.edenchengsc.covidstatsaroundus.controllers;
 
 import com.edenchengsc.covidstatsaroundus.models.County;
+import com.edenchengsc.covidstatsaroundus.models.RiskMap;
 import com.edenchengsc.covidstatsaroundus.models.State;
 import com.edenchengsc.covidstatsaroundus.service.CovidDataService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+import java.io.FileWriter;
 import java.util.*;
 
 @Controller
@@ -43,32 +49,66 @@ public class GraphController {
     @GetMapping("/vaccineStateBarChart")
     public String vaccineStateBarChart(Model model){
 
-        List<State> allCountyStats = covidDataService.getStateStats();
+        List<State> allStateStats = covidDataService.getStateStats();
 
-        Collections.sort(allCountyStats, new Comparator<State>() {
+        Collections.sort(allStateStats, new Comparator<State>() {
             @Override
             public int compare(State o1, State o2) {
                 return Math.round((o2.getMetrics().getVaccinationsCompletedRatio() - o1.getMetrics().getVaccinationsCompletedRatio()) * 100);
             }
         });
 
-        Map<String, Integer> data = new LinkedHashMap<>();
+        Map<String, Integer> vaccinationsCompletedRatioData = new LinkedHashMap<>();
+        Map<String, Integer> vaccinationsInitiatedRatioData = new LinkedHashMap<>();
 
-        for(State state : allCountyStats){
-            data.put(state.getState(), Math.round(state.getMetrics().getVaccinationsCompletedRatio()*100));
+        for(State state : allStateStats){
+            //System.out.println("State: " + state.getState() + "  - " + Math.round(state.getMetrics().getVaccinationsCompletedRatio()*100) );
+            vaccinationsInitiatedRatioData.put(state.getState(), Math.round(state.getMetrics().getVaccinationsInitiatedRatio()*100));
+            vaccinationsCompletedRatioData.put(state.getState(), Math.round(state.getMetrics().getVaccinationsCompletedRatio()*100));
+
         }
 
-        model.addAttribute("keySet", data.keySet());
-        model.addAttribute("values", data.values());
+        model.addAttribute("keySet", vaccinationsCompletedRatioData.keySet());
+        model.addAttribute("vaccinationsInitiatedRatioValues", vaccinationsInitiatedRatioData.values());
+        model.addAttribute("vaccinationsCompletedRatioValues", vaccinationsCompletedRatioData.values());
 
         return "vaccineStateBarChart";
     }
 
-    @GetMapping("/pieChart")
-    public String pieChart(Model model){
-        model.addAttribute("pass", 90);
-        model.addAttribute("fail", 10);
-        return "pieChart";
+    @GetMapping("/riskMapChart")
+    public String riskMapChart(Model model) throws JsonProcessingException {
+//        List<State> allStateStats = covidDataService.getStateStats();
+//
+//        Map<String, Integer> riskLevelAllState = new LinkedHashMap<>();
+//
+//        List<RiskMap> reislList = new ArrayList<>();
+//
+//        for(State state : allStateStats){
+//            System.out.println("State: " + state.getState() + "  - " + state.getRiskLevels().getOverall());
+//            riskLevelAllState.put(state.getState(), state.getRiskLevels().getOverall());
+//
+//            RiskMap rs = new RiskMap();
+//            rs.setCode(state.getState());
+//            rs.setValue(state.getRiskLevels().getOverall());
+//            reislList.add(rs);
+//        }
+
+//
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String json = "";
+//        try {
+//            json= objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(reislList);
+//            System.out.println(json);
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+
+
+
+        return "riskMapChart";
+
+
     }
 
 
